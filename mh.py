0645741374e968f3f10395ba052aeb945c162e36
Chris Angelico <rosuav@gmail.com>
2015-03-23 20:37:56 +1100

    Allow actual creation of database entries for membership permissions

--- mh.py
+++ mh.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, g, Markup
 import mistune as md
 import psycopg2
 import subprocess
+import os
+import smtplib
 import config # Local config variables and passwords, not in source control
 app = Flask(__name__)
 
@@ -36,12 +38,51 @@ def campaign(id):
 
 @app.route("/memb/<hash>")
 def membership(hash):
-	# TODO: Verify the hash against an email address and check it's still subscribed.
-	if False:
+	db = get_db()
+	cur = db.cursor()
+	cur.execute("select email from membership where hash=%s", (hash,))
+	emails = subprocess.Popen(["sudo", "list_members", "committee"], stdout=subprocess.PIPE).communicate()[0].split("\n")
+	for row in cur:
+		if row[0] in emails: break
+	else:
+		# Not found - wrong hash, or no longer subscribed
 		return "<!doctype html><html><head><title>Invalid access code</title></head><body><h1>Invalid access code</h1></body></html>"
 	# PyDrive isn't set up in the system Python, so we explicitly invoke a different Python to do the work for us.
 	p = subprocess.Popen(["/usr/local/bin/python2.7","/home/gideon/MembershipAccess/membaccess.py","html"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	return p.communicate()[0] # TODO: Make sure this can't block
+
+@app.route("/memb")
+def membership_setup():
+	db = get_db()
+	# create table membership (email varchar primary key,hash varchar not null unique);
+	emails = subprocess.Popen(["sudo", "list_members", "committee"], stdout=subprocess.PIPE).communicate()[0].split("\n")
+	cur = db.cursor()
+	cur.execute("select email from membership")
+	for email in cur: emails.remove(email)
+	emails.remove("")
+	if emails:
+		s=smtplib.SMTP("localhost")
+		for email in emails:
+			hash = os.urandom(8).encode('hex')
+			cur.execute("insert into membership values (%s, %s)", (email, hash))
+			s.sendmail("no-reply@gilbertandsullivan.org.au",["rosuav@gmail.com"],"""Content-Type: text/plain; charset="us-ascii"
+From: no-reply@gilbertandsullivan.org.au
+To: %s
+Subject: Membership database access
+
+Access to the G&S Society membership database is by personalized links.
+Your link is: http://i.rosuav.com/memb/%s
+Please retain this email for your records.
+
+Thanks!
+
+Chris Angelico
+Domainmaster, gilbertandsullivan.org.au
+""" % (email, hash))
+		db.commit()
+		return "Sent emails to:<ul><li>"+"<li>".join(emails)+"</ul>"
+	db.commit()
+	return "No new emails to send."
 
 if __name__ == "__main__":
 	import logging
