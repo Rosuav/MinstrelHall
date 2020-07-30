10c262fe40e83bc3584b0995bb3c165fcf91615f
Chris Angelico <rosuav@gmail.com>
2020-07-30 21:35:24 +1000

    Migrate away from CONG/RATS to a GG flag

--- datasets.py
+++ datasets.py
@@ -60,8 +60,7 @@ BINGO = {
 			'''"Let's see if this works..." *BEWM*\n\n![fxnSnipe](https://static-cdn.jtvnw.net/emoticons/v1/925941/1.0)''',
 			'''"I'll fix that later..."\n\n![devicatZZZ](https://static-cdn.jtvnw.net/emoticons/v1/1291578/1.0)''',
 			"""Singing along with music\n\n![acsHiFi](https://static-cdn.jtvnw.net/emoticons/v1/171227/1.0)""",
-			"""Code refactor actually works\n\n![kaypikeCONG](https://static-cdn.jtvnw.net/emoticons/v1/579388/1.0)
-				![kaypikeRATS](https://static-cdn.jtvnw.net/emoticons/v1/579386/1.0)""",
+			"""Code refactor actually works\n\n![fxnGG](https://static-cdn.jtvnw.net/emoticons/v1/1515667/1.0)""",
 			"""A bad pun is cracked\n\n![rosuavMuted](https://static-cdn.jtvnw.net/emoticons/v1/390027/1.0)""",
 			"""A reference is made to Alice, Disney or G&S""",
 		],
